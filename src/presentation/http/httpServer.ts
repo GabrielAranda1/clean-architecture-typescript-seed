8dc0e8ff78a836fae453c106ecd6777b4c6721e9
GabrielAranda1 <per.gabrielaranda@gmail.com>
2023-08-08 20:06:20 -0300

    style: added new style rules

--- src/presentation/http/httpServer.ts
+++ src/presentation/http/httpServer.ts
@@ -1,13 +1,13 @@
-import { env } from '@/main/env';
-import express from 'express';
+import { env } from '@/main/env'
+import express from 'express'
 
 function startHttpServer() {
-  const server = express();
+  const server = express()
 
-  server.use(express.json());
-  server.use(express.urlencoded({ extended: true }));
+  server.use(express.json())
+  server.use(express.urlencoded({ extended: true }))
 
-  server.listen(env.port, () => console.log(`Server running at port ${env.port}`));
+  server.listen(env.port, () => console.log(`Server running at port ${env.port}`))
 }
 
 export { startHttpServer }
